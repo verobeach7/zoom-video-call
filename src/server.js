b02f5e15f648f171b2293848ef155ed7dc09c071
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-23 21:25:09 +0900

    Zoom Clone: Chat

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
-// catchall url
 app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
@@ -17,17 +16,19 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+// fake database: socket을 넣어주어 각각의 socket에 동일한 작업을 할 수 있게함.
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser ✅");
-  // .send는 wss서버 method가 아닌 socket의 method임
   socket.on("close", () => {
     console.log("Disconnected from the Browser ❌");
   });
   socket.on("message", (message) => {
-    // blob(buffer)로 들어온 것을 string으로 변경
-    console.log(message.toString());
+    // 연결된 socket 모두에게 동일한 작업 수행
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
   });
-  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
