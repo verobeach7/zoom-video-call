59eb6f6e3aaef8002e466b3aee6a2832722cf32b
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-23 17:21:58 +0900

    Zoom Clone: WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+// http는 node.js에 이미 설치되어 있어 따로 설치가 필요하지 않음
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -10,4 +13,14 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+// express.js를 이용하여 http 서버를 생성
+const server = http.createServer(app);
+
+// http와 ws을 둘 다 사용할 수 있도록 하기 위하여 이렇게 하는 것
+// http 서버 위에 ws 서버를 만든 것
+// http protocol이 필요하지 않으면 ws만 만들어주면 됨
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
+// app.listen(3000, handleListen);
