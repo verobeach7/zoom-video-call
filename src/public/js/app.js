d7c24d943b47b1c26a774e6df54f385fc3277966
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-23 22:22:45 +0900

    Zoom Clone: Nicknames and Chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,22 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// socket.send가 string만 보낼 수 있으므로 json객체를 string으로 바꿔주는 함수 작성
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -17,8 +26,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -16,18 +16,36 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
-// fake database: socket을 넣어주어 각각의 socket에 동일한 작업을 할 수 있게함.
 const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  // 아직 nickname을 설정하지 않은 사람에 대한 대처
+  socket["nickname"] = "Anonymous";
   console.log("Connected to Browser ✅");
   socket.on("close", () => {
     console.log("Disconnected from the Browser ❌");
   });
-  socket.on("message", (message) => {
-    // 연결된 socket 모두에게 동일한 작업 수행
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    // WebSocket을 통해 전달받은 string을 다시 객체로 바꿔주는 것
+    const message = JSON.parse(msg);
+    // break를 걸지 않으면 뒤에 case도 다 실행됨.
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        // socket은 객체이기 때문에 원하는 것을 추가 가능!!!
+        socket["nickname"] = message.payload;
+        break;
+    }
+    /* if (message.type === "new_message") {
+      sockets.forEach((aSocket) => aSocket.send(message.payload));
+    } else if (message.type === "nickname") {
+      console.log(message.payload);
+    } */
   });
 });
 
