0e54d345639a393ae1d837e1e904cc88541eb1ee
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-26 20:20:02 +0900

    Zoom Clone: Answers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -17,20 +17,13 @@ let myPeerConnection;
 
 async function getCameras() {
   try {
-    // 모든 미디어장치
     const devices = await navigator.mediaDevices.enumerateDevices();
-    // videoinput만 filter
     const cameras = devices.filter((device) => device.kind === "videoinput");
-    // console.log("cameras", cameras);
-    // 현재 사용하고 있는 카메라 정보
     const currentCamera = myStream.getVideoTracks();
-    // console.log("currentCamera", currentCamera);
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
-      // console.log("camera", camera);
-      // 초기 사용되는 카메라가 선택되어있게 함.
       if (currentCamera.label === camera.label) {
         option.selected = true;
       }
@@ -42,12 +35,10 @@ async function getCameras() {
 }
 
 async function getMedia(deviceId) {
-  // 사용자가 지정한 장비가 없을 때 constraints
   const initialConstraints = {
     audio: true,
     video: { facingMode: "user" },
   };
-  // 사용자가 카메라 장비를 선택했을 때 constraints
   const cameraConstraints = {
     audio: true,
     video: {
@@ -56,14 +47,11 @@ async function getMedia(deviceId) {
       },
     },
   };
-  // promise 구문은 try{}catch(err){} 사용
   try {
-    // MediaStream 객체를 반환. 사용 가능한 미디어 장치 정보 가져옴. 미디어 접근을 요청함.
     myStream = await navigator.mediaDevices.getUserMedia(
       deviceId ? cameraConstraints : initialConstraints
     );
     myFace.srcObject = myStream;
-    // 사용자가 지정한 카메라가 없을 때. 즉, 처음 시작할 때만 카메라 정보를 가져옴.
     if (!deviceId) {
       getCameras();
     }
@@ -73,17 +61,13 @@ async function getMedia(deviceId) {
 }
 
 function handleMuteClick() {
-  // myStream.getAudioTracks()는 장치정보 배열을 가져옴. 배열 내의 정보를 활용.
-  // console.log(myStream.getAudioTracks());
   myStream
     .getAudioTracks()
     .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
-    // 음소거 되어있지 않으면...음소거하고 버튼은 음소거 풀어라
     muteBtn.innerText = "Unmute";
     muted = true;
   } else {
-    // 음소거 되어있으면...음소거 풀고 버튼은 음소거 해라
     muteBtn.innerText = "Mute";
     muted = false;
   }
@@ -93,11 +77,9 @@ function handleCameraClick() {
     .getVideoTracks()
     .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
-    // 카메라 꺼져 있으면...켜주고 버튼은 꺼라로 설정
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
   } else {
-    // 카메라 켜져 있으면...꺼주고 버튼은 켜라로 설정
     cameraBtn.innerText = "Turn Camera On";
     cameraOff = true;
   }
@@ -116,51 +98,59 @@ muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
 
-// Welcome Form(Join a room)
+/* Welcome Form(Join a room) */
 
 const welcome = document.getElementById("welcome");
 
 welcomeForm = welcome.querySelector("form");
 
-async function startMedia() {
+async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
   await getMedia();
   makeConnection();
 }
 
-function handleWelcomeSubmit(event) {
+async function handleWelcomeSubmit(event) {
   event.preventDefault();
   const input = welcomeForm.querySelector("input");
-  socket.emit("join_room", input.value, startMedia);
+  // initCall()을 emit의 함수로 넣어주면 실행속도가 너무 빨라서 offer 이벤트를 받을 때 아직 myPeerConnection이 없어서 에러가 남
+  // 이를 해결하기 위해 async await으로 initCall()을 사전에 실행해줌
+  await initCall();
+  socket.emit("join_room", input.value);
   roomName = input.value;
   input.value = "";
 }
 
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
-// Socket Code
+/* Socket Code */
 
-// 누군가 들어오면 이곳을 통해서 offer를 보낼 수 있음
+// Browser A
 socket.on("welcome", async () => {
-  // offer: 다른 브라우저가 이곳으로 올 수 있도록 초대장을 만드는 것
   const offer = await myPeerConnection.createOffer();
-  // console.log(offer);
   myPeerConnection.setLocalDescription(offer);
   console.log("sent the offer");
-  // send offer(socket.io server가 어떤 방으로 offer를 보내줘야 할지 알아야하기 때문에 roomName도 보내줘야 함)
   socket.emit("offer", offer, roomName);
 });
 
-socket.on("offer", (offer) => {
-  console.log(offer);
+// Browser B
+socket.on("offer", async (offer) => {
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
 });
 
-// RTC Code
+socket.on("answer", (answer) => {
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+/* RTC Code */
 
 function makeConnection() {
   myPeerConnection = new RTCPeerConnection();
-  // 나의 track에서 video와 audio를 가져와서 각 track을 peerConnection에 추가해줌
+  // addStream()은 오랜된 거라 사용하지 않음. 대신 addTrack 사용!!!
   myStream
     .getTracks()
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
