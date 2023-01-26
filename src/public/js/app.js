d7958d8a35b615ed1ffd6309b6844a86cba9f9cc
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-26 19:20:16 +0900

    Zoom Clone: Rooms & Offers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,9 +5,15 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -15,15 +21,15 @@ async function getCameras() {
     const devices = await navigator.mediaDevices.enumerateDevices();
     // videoinput만 filter
     const cameras = devices.filter((device) => device.kind === "videoinput");
-    console.log("cameras", cameras);
+    // console.log("cameras", cameras);
     // 현재 사용하고 있는 카메라 정보
     const currentCamera = myStream.getVideoTracks();
-    console.log("currentCamera", currentCamera);
+    // console.log("currentCamera", currentCamera);
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
-      console.log("camera", camera);
+      // console.log("camera", camera);
       // 초기 사용되는 카메라가 선택되어있게 함.
       if (currentCamera.label === camera.label) {
         option.selected = true;
@@ -66,8 +72,6 @@ async function getMedia(deviceId) {
   }
 }
 
-getMedia();
-
 function handleMuteClick() {
   // myStream.getAudioTracks()는 장치정보 배열을 가져옴. 배열 내의 정보를 활용.
   // console.log(myStream.getAudioTracks());
@@ -101,8 +105,63 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  if (muted) {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = false));
+  } else {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = true));
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// Welcome Form(Join a room)
+
+const welcome = document.getElementById("welcome");
+
+welcomeForm = welcome.querySelector("form");
+
+async function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, startMedia);
+  roomName = input.value;
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+// 누군가 들어오면 이곳을 통해서 offer를 보낼 수 있음
+socket.on("welcome", async () => {
+  // offer: 다른 브라우저가 이곳으로 올 수 있도록 초대장을 만드는 것
+  const offer = await myPeerConnection.createOffer();
+  // console.log(offer);
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  // send offer(socket.io server가 어떤 방으로 offer를 보내줘야 할지 알아야하기 때문에 roomName도 보내줘야 함)
+  socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", (offer) => {
+  console.log(offer);
+});
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  // 나의 track에서 video와 audio를 가져와서 각 track을 peerConnection에 추가해줌
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
